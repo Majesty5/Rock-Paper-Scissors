79192613dece742f96942fac7633d80bdf83d2b1
Majesty5 <rashidathomas1@yahoo.com>
2020-02-26 23:07:06 -0500

    Added logic to decide the winner

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import PlayerCard from './PlayerCard'
 class Game extends Component {
   constructor () {
     super ();
-this.signs = ["rock", "scissors", "paper"];
+this.signs = ["rock", "paper", "scissors"];
 this.state = {
   playerOne: "rock",
   playerTwo: "scissors",
@@ -19,11 +19,53 @@ this.state = {
     })
   }
 
-  // decideWinner = () => {
-  //
-  //   })
-  // }
+// *********** My attempt to create code. Gibberish, but the logic works.
+//   decideWinner = () => {
+//     function playerOneW(props) {
+//     return <h1>Woot Woot Player 1</h1>;
+//   }
+//
+//   function playerTwoW(props) {
+//     return <h1>Aw Snap Player 2</h1>;
+//   }
+//
+//   function tiedGame(props) {
+//     return <h1>Let's Have a Do-Over</h1>;
+//   }
+//   function winnyWin(props) {
+//     if (playerOne > playerTwo);
+//      {
+//       return <playerOneW />;
+//     }
+//     if (playerTwo > playerOne);
+//     return <playerTwoW />;
+//   }
+//     }
+//     if (playerTwo == playerOne);
+//     return <tiedGame />;
+//   }
+//
+//     })
+//   }
+//********************* See the correct code below
 
+decideWinner = ()=> {
+  const playerOne = this.state.playerOne
+  const playerTwo = this.state.playerTwoW
+
+  if (playerOne === playerTwo) {
+    return "It's A Tie!"
+  } else if ((playerOne === "rock" && playerTwo === "scissors")||
+            (playerOne === "paper" && playerTwo === "rock")||
+            (playerOne === "scissors" && playerTwo === "paper")){
+
+    return "Player One Wins!"
+
+  }
+    else {
+      return "Player Two Wins!"
+    }
+}
 
   render (){
     return (
@@ -32,7 +74,7 @@ this.state = {
         <PlayerCard sign={this.state.playerOne}/>
         <PlayerCard sign={this.state.playerTwo}/>
         </div>
-        <div className="winner"> The Champiyon! </div>
+        <div className="winner">{this.decideWinner ()}</div>
         <button type="button" onClick={this.playGame}> Get This Work </button>
       </div>
     )
